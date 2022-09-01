70c4a4153e04b7525cc11859d56252a0c709ac59
chan-wired <chan@wired.company>
2022-09-01 18:09:26 +0900

    d

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -5,19 +5,19 @@ if [[ -f ${PACKAGE_JSON_FILE} ]]; then
 
   case "$1" in
     --major)
-      npx standard-version --release-as major
+      npx standard-version --release-as major --skip.commit --skip.tag
       ;;
     --minor)
-      npx standard-version --release-as minor
+      npx standard-version --release-as minor --skip.commit --skip.tag
       ;;
     --patch)
-      npx standard-version --release-as patch
+      npx standard-version --release-as patch --skip.commit --skip.tag
       ;;
     --pre-minor)
-      npx standard-version --release-as minor --prerelease stage
+      npx standard-version --release-as minor --prerelease stage --skip.commit --skip.tag
       ;;
     --pre-patch)
-      npx standard-version --release-as patch --prerelease stage
+      npx standard-version --release-as patch --prerelease stage --skip.commit --skip.tag
       ;;
     *)
       echo "version not changed"
@@ -25,6 +25,16 @@ if [[ -f ${PACKAGE_JSON_FILE} ]]; then
       ;;
     esac
 
-  # changelog.html 파일 생성
+  ### changelog.html 파일 생성
   # npm run showdown
+
+  ### get version
+  # VERSION=$(cat ${PACKAGE_JSON_FILE} | python3 -c "import sys, json; print(json.load(sys.stdin)['version'])")
+  # echo "${VERSION}"
+
+  ### git commit and tag
+  # git add -u
+  ### TODO: version prefix를 standard-version에서 가져올 방법을 찾아봐야할듯
+  # git commit -m "chore(release): ${VERSION}"
+  # git tag "v${VERSION}"
 fi
